4d7ed504b960d658ce1df47a857f0e5dbd59b0f9
rajatjayswal1410 <rajatjayswalRnw@gmail.com>
2023-03-23 17:58:59 +0530

    22 march

---
+++ app.js
@@ -0,0 +1,95 @@
+
+        // function spt(s,c){
+        //     let i = 0
+        //     let arr =[]
+        //     let tmp = ""
+        //     while (i < s.length) {
+        //         console.log(tmp);
+        //         if(s[i] === c){
+        //             arr.push(tmp)
+        //             tmp = ""
+        //         }else{
+        //             tmp = tmp + s[i]
+        //         }
+        //         i++
+        //     }
+        //     arr.push(tmp)
+        //     return arr
+        // }
+
+        // const t = spt();
+
+
+
+
+
+
+
+        // function callBack(x){
+        // console.log(x);
+        // }
+
+        // data.map(callBack)
+
+        // function map(a){
+        //     a()
+        // }
+        // const m = () => {
+
+        // }
+
+
+        // const data = [
+        //     {name:"Rajat", age:22},
+        //     {name:"soham", age:22},
+        //     {name:"bhavadeep", age:22},
+        //     {name:"nishant", age:22},
+        //     {name:"darshak", age:22},
+        // ]
+
+
+        // data.map(function(x,index){
+        //     console.log(x,index);
+        // })
+
+
+
+        // function Nap(arr, fun){
+        //     console.log(arr);
+        //     console.log(fun);
+        //     let i =0
+        //     while (i < arr.length) {
+        //         fun(arr[i], i)
+        //         i++
+        //     }
+        // }
+
+
+        // Nap(data, function (w, index){
+        //     console.log(w,index);
+        // })
+
+
+        // 22-03-2023
+
+        // function Recurtion(a) {
+        //     if (a >= 10){
+        //         return a
+        //     }else{
+        //         console.log(a);
+        //         a++
+        //         Recurtion(a)
+        //     }
+        // }
+
+        // Recurtion(0)
+
+
+        // function Closer(a,b){
+        //     return (c) => {
+        //         return a+b+c
+        //     }
+        // }
+
+
+        // const result = Closer(1,2)
